23daced31f0045175cf5143b0b025fc408d1a4fd
darroyolpz <darroyolpz@users.noreply.github.com>
2020-03-18 11:29:09 +0100

    Encoding UTF-8

--- outlook sender.py
+++ outlook sender.py
@@ -2,23 +2,23 @@
 
 import pandas as pd
 
-excel_file = 'Emails data.xlsx'
+excel_file = 'Contacts 645.xlsx'
 df = pd.read_excel(excel_file)
 
-max_list = 10
+max_list = 100
 email_list = ''
 
 for row in df['Email']:
 	list_len = email_list.count(';')
 	if list_len < max_list:
-		email_list = email_list + str(row) + '; '
+		email_list = email_list + str(row) + ';'
 	else:
-		print(email_list)
+		#print('List len:', list_len)
+		print(email_list.encode('utf-8'))
 		print('\n')
-		email_list = str(row) + '; '
+		email_list = str(row) + ';'
 
 print('Last one:')
 print('\n')
 print(email_list)
 print('\n')
-
